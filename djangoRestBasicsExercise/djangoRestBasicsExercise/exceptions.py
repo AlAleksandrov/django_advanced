96891d5e42c62348b13711505579ff2f4acc2cbd
AlAleksandrov <algal.bg@gmail.com>
2026-03-27 19:14:14 +0200

    feat: added more functionalities

--- djangoRestBasicsExercise/djangoRestBasicsExercise/exceptions.py
+++ djangoRestBasicsExercise/djangoRestBasicsExercise/exceptions.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 
